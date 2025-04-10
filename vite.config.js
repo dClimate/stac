cf86d7550e88d992912b79456df0b1d403667118
Faolain <Faolain@users.noreply.github.com>
2025-04-10 18:19:54 -0400

    fix: remove useless changes

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-
-export default defineConfig({
-	// ... other config
-	plugins: [
-		nodePolyfills({
-			protocolImports: true,
-			// You might not even need nodePolyfills if alias works,
-			// but keeping it might polyfill other things like 'buffer'.
-		}),
-	],
-	resolve: {
-		alias: {
-			crypto: "crypto-browserify", // Add this alias
-		},
+export default {
+	base: "/",
+	server: {
+		port: 3000,
+		open: true,
 	},
-	// ... rest of config
-});
+	build: {
+		outDir: "dist",
+		sourcemap: true,
+	},
+};
